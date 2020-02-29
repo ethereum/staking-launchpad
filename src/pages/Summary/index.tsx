3389714efb72e07632b1733380fa52bc7c3ae1a4
Chris Battenfield <chrisbattenfield@gmail.com>
2020-02-29 00:59:16 -0800

    WIP tx ui

--- src/pages/Summary/index.tsx
+++ src/pages/Summary/index.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Box, CheckBox, Heading, Text } from "grommet";
+import { Spinning } from 'grommet-controls';
+import { FormNext } from "grommet-icons";
 import styled from "styled-components";
 import { useWeb3React } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
@@ -8,6 +10,7 @@ import { AbstractConnector } from "@web3-react/abstract-connector";
 import Web3 from "web3";
 import { Eth } from "web3-eth";
 import { SendOptions } from "web3-eth-contract";
+import { Redirect } from "react-router-dom";
 import { StoreState } from "../../store/reducers";
 import { keyFile, ProgressStep } from "../../store/actions";
 import { Paper } from "../../components/Paper";
@@ -24,6 +27,7 @@ import { prefix0X } from "../../utils/prefix0x";
 import { contractAbi } from "../../contractAbi";
 import { rainbowMutedColors } from "../../styles/styledComponentsTheme";
 import { contractAddress, pricePerValidator } from "../../enums";
+import { routesEnum } from "../../Routes";
 
 // DEPOSIT CONTRACT VARIABLES(public for transparency)
 const CONTRACT_ADDRESS = contractAddress;
@@ -46,6 +50,9 @@ const _SummaryPage = ({
   const [earlyAdopt, setEarlyAdopt] = useState(false);
   const [nonReverse, setNonReverse] = useState(false);
   const [noPhish, setNoPhish] = useState(false);
+  const [txMining, setTxMining] = useState(false)
+  const [redirectToCongratulations, setRedirectToCongratulations] = useState(false)
+  const [transactionList, setTransactionList] = useState([])
   const allChecked = losePhrase && earlyAdopt && nonReverse && noPhish;
   const validatorKeys = keyFiles.map(file => file.pubkey);
   const validatorCount = keyFiles.length;
@@ -75,6 +82,55 @@ const _SummaryPage = ({
       </Box>
     </Paper>
   );
+
+  interface tx {
+    pubkey: string
+    txSent: boolean,
+    status?: string,
+    etherscanLink?: string
+  }
+
+  // @ts-ignore
+  const transactionSection = (transactionList): JSX.Element => (
+    <Box>
+      {transactionList.map((tx: tx) => {
+        console.log(transactionList, 'from txsection')
+        const { pubkey, txSent, status, etherscanLink } = tx
+        let etherscanLinkText = etherscanLink || "/"
+        const transactionStatus = txSent ? "pending transaction" : "rejected signing"
+        return (
+          <div className="flex flex-row space-between mt20">
+            <InfoBox>{pubkey}</InfoBox>
+            <InfoBox>{status ? status : transactionStatus}</InfoBox>
+            <InfoBox>{
+              <Link external to={etherscanLinkText} primary>follow tx on etherscan <FormNext color="blueDark" /></Link>
+            }
+            </InfoBox>
+
+          </div>
+        )
+      })}
+    </Box>
+  );
+
+
+  const transactionHeading = (): JSX.Element => {
+    console.log('in transactionSection')
+
+    return (
+      <Box width="100%" className="flex flex-row space-between">
+        <SummarySection>
+          <Text weight="bold">Validator Key</Text>
+        </SummarySection>
+        <SummarySection>
+          <Text weight="bold">Transaction status</Text>
+        </SummarySection>
+        <SummarySection>
+          <Text weight="bold">Transaction Link</Text>
+        </SummarySection>
+      </Box>
+    )
+  }
 
   const renderKeyList = (): JSX.Element => (
     <Paper className="mt20">
@@ -112,8 +168,8 @@ const _SummaryPage = ({
         <Text>
           You are responsible for the transaction. Fraudulent websites might
           lure you into sending the {pricePerValidator} ETH to them, instead of
-          the official deposit contract. Please check that the address you are
-          sending the transaction to is the correct address.
+              the official deposit contract. Please check that the address you are
+              sending the transaction to is the correct address.
         </Text>
         <Link to="https://www.google.com" external className="mt10" primary>
           Learn here how to do it safely
@@ -128,6 +184,20 @@ const _SummaryPage = ({
       </AcknowledgementSection>
     </div>
   );
+
+  const handleTxChange = (pubkey: string, txValues: {}): any[] => {
+    // @ts-ignore
+    return setTransactionList((oldTxList: tx) => (oldTxList.map(oldTx => {
+      console.log('old', oldTxList)
+      console.log('oldtx', oldTx)
+
+      if (oldTx.pubkey === pubkey) {
+        console.log('val', txValues)
+        return { ...oldTx, txValues }
+      }
+      return oldTx;
+    })))
+  }
 
   const handleTransaction = async (depositFile: keyFile): Promise<void> => {
     const {
@@ -159,7 +229,15 @@ const _SummaryPage = ({
         .send(transactionParameters)
         // Event for when the user confirms the tx
         .on("transactionHash", (txId: string): void => {
+          setTxMining(true)
           console.log("transaction id", txId);
+          const txObj = {
+            pubkey,
+            txSent: true,
+            etherscanLink: `https://goerli.etherscan.io/tx/${txId}`
+          }
+          // @ts-ignore
+          setTransactionList((oldTxList) => [...oldTxList, txObj])
           // TODO(tx UI feature): return txId
         })
         // Event is for when the tx is mined
@@ -168,9 +246,11 @@ const _SummaryPage = ({
           (confirmation: number, receipt: { status: {} }): void => {
             if (confirmation === 0) {
               console.log("receipt: ", receipt);
+              handleTxChange(pubkey, { status: receipt.status })
               if (receipt.status) {
                 console.log("receipt status: ", receipt.status);
                 // TODO(tx UI feature): return status
+
               } else {
                 console.log("error: receipt status not received");
               }
@@ -180,6 +260,12 @@ const _SummaryPage = ({
     } catch (rejected) {
       console.log("user rejected transaction: ", rejected);
       // TODO(tx UI): return rejected status
+      const txObj = {
+        pubkey,
+        txSent: false,
+      }
+      // @ts-ignore
+      setTransactionList((oldTxList) => [...oldTxList, txObj])
     }
   };
 
@@ -190,6 +276,10 @@ const _SummaryPage = ({
       handleTransaction(validator);
     });
   };
+
+  if (redirectToCongratulations) {
+    return <Redirect to={routesEnum.CongratulationsPage} />
+  }
 
   if (progress !== ProgressStep.SUMMARY) {
     return routeToCorrectProgressStep(progress);
@@ -225,6 +315,28 @@ const _SummaryPage = ({
     );
   }
 
+  // TODO: add in grid
+  if (txMining) {
+    return (
+      <WorkflowPageTemplate title="Summary"
+        backgroundColor={rainbowMutedColors[5]}>
+        <Paper>
+          <Box align="center">
+            <Text size="large" className="my10">Your transactions have started processing</Text>
+            <Text size="medium" className="my20">Please confrim your transaction for each validator key you have generated</Text>
+            <Spinning size="large" />
+            {transactionList && (
+              <Box className="mt20" width="100%">
+                {transactionHeading()}
+                {transactionSection(transactionList)}
+              </Box>
+            )}
+          </Box>
+        </Paper>
+      </WorkflowPageTemplate>
+    )
+  }
+
   return (
     <WorkflowPageTemplate
       title="Summary"
@@ -241,7 +353,7 @@ const _SummaryPage = ({
             disabled={!allChecked}
             label={`SIGN ${validatorCount} TRANSACTION${
               validatorCount > 1 ? "S" : ""
-            } AND DEPOSIT ${validatorCount * pricePerValidator} ETH`}
+              } AND DEPOSIT ${validatorCount * pricePerValidator} ETH`}
             onClick={handleDepositClick}
           />
         </Link>

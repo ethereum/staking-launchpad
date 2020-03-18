32ba0805e0a931e972545fbae8dea975bf505e4c
George Weiler <georgejweiler@gmail.com>
2020-03-18 08:49:29 -0700

    validate keyfile for correct value types and length (#15)
    * validate keyfile for correct value types and length
    
    * add util function for running BLS verification on deposit files
    
    * perform bls check on validator keys
    
    * rm console logs
    
    * add strict checking for json files

--- src/pages/UploadValidator/index.tsx
+++ src/pages/UploadValidator/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { WorkflowPageTemplate } from "../../components/WorkflowPage/WorkflowPageTemplate";
 import { Paper } from "../../components/Paper";
 import { StoreState } from "../../store/reducers";
@@ -14,7 +14,10 @@ import { Box, Text } from "grommet";
 import styled from "styled-components";
 import { routeToCorrectProgressStep } from "../../utils/RouteToCorrectProgressStep";
 import { Button } from "../../components/Button";
-import {rainbowMutedColors} from "../../styles/styledComponentsTheme";
+import { rainbowMutedColors } from "../../styles/styledComponentsTheme";
+import { verifySignature } from "../../utils/verifySignature";
+import _every from "lodash/every";
+import { initBLS } from "@chainsafe/bls";
 
 const BackBtn = styled(Text)`
   color: ${p => p.theme.gray.medium};
@@ -24,13 +27,79 @@ const BackBtn = styled(Text)`
     color: ${p => p.theme.gray.dark};
   }
 `;
+const Instructions = styled.span`
+  font-weight: bold;
+  :hover {
+    color: ${p => p.theme.red.light};
+    cursor: pointer;
+  }
+`;
+const ErrorText = styled(Text)`
+  width: 500px;
+  margin: 20px auto auto;
+  text-align: center;
+`;
 
 interface Props {
   updateKeyFiles(files: keyFile[]): void;
+
   keyFiles: keyFile[];
   updateProgress: (step: ProgressStep) => void;
   progress: ProgressStep;
 }
+
+const validateKeyFile = async (files: keyFile[]): Promise<boolean> => {
+  await initBLS();
+
+  if (!Array.isArray(files)) return false;
+  if (files.length <= 0) return false;
+
+  const keyfileStatuses: boolean[] = files.map(file => {
+    const {
+      pubkey,
+      withdrawal_credentials,
+      amount,
+      signature,
+      deposit_data_root
+    } = file;
+
+    // check existence of required keys
+    if (
+      !pubkey ||
+      !withdrawal_credentials ||
+      !amount ||
+      !signature ||
+      !deposit_data_root
+    ) {
+      return false;
+    }
+
+    // check type of values
+    if (
+      typeof pubkey !== "string" ||
+      typeof withdrawal_credentials !== "string" ||
+      typeof amount !== "number" ||
+      typeof signature !== "string" ||
+      typeof deposit_data_root !== "string"
+    ) {
+      return false;
+    }
+
+    // check length of strings
+    if (
+      pubkey.length !== 96 ||
+      withdrawal_credentials.length !== 64 ||
+      signature.length !== 192 ||
+      deposit_data_root.length !== 64
+    ) {
+      return false;
+    }
+
+    // perform BLS check
+    return verifySignature(pubkey, signature, deposit_data_root);
+  });
+  return _every(keyfileStatuses);
+};
 
 export const _UploadValidatorPage = ({
   keyFiles,
@@ -39,6 +108,33 @@ export const _UploadValidatorPage = ({
   progress
 }: Props): JSX.Element => {
   const [fileAccepted, setFileAccepted] = useState(false);
+  const [invalidKeyFile, setInvalidKeyFile] = useState(false);
+
+  const onDrop = useCallback(
+    acceptedFiles => {
+      if (acceptedFiles.length === 1) {
+        setInvalidKeyFile(false);
+        const reader = new FileReader();
+        reader.onload = async event => {
+          if (event.target) {
+            try {
+              const fileData = JSON.parse(event.target.result as string);
+              if (await validateKeyFile(fileData as keyFile[])) {
+                setFileAccepted(true);
+                updateKeyFiles(fileData);
+              } else {
+                setInvalidKeyFile(true);
+              }
+            } catch (e) {
+              setInvalidKeyFile(true);
+            }
+          }
+        };
+        reader.readAsText(acceptedFiles[0]);
+      }
+    },
+    [setFileAccepted, updateKeyFiles]
+  );
 
   const handleSubmit = () => {
     updateProgress(ProgressStep.CONNECT_WALLET);
@@ -53,21 +149,31 @@ export const _UploadValidatorPage = ({
   }
 
   return (
-    <WorkflowPageTemplate title="Upload Deposits" backgroundColor={rainbowMutedColors[3]}>
+    <WorkflowPageTemplate
+      title="Upload Deposits"
+      backgroundColor={rainbowMutedColors[3]}
+    >
       <Paper>
         <StyledDropzone
           fileAccepted={fileAccepted}
-          setFileAccepted={setFileAccepted}
-          updateKeyFiles={updateKeyFiles}
           keyFiles={keyFiles}
+          onDrop={onDrop}
         />
+        {invalidKeyFile && (
+          <ErrorText color="redMedium">
+            There was an error processing your key file. Please follow the
+            instructions{" "}
+            <Instructions onClick={handleGoBack}>here</Instructions> to generate
+            your file.
+          </ErrorText>
+        )}
 
         <Box align="center" pad="large">
           <BackBtn onClick={handleGoBack}>Go Back</BackBtn>
           <Button
             width={300}
             rainbow
-            disabled={!fileAccepted}
+            disabled={!fileAccepted || invalidKeyFile}
             label="Continue"
             onClick={handleSubmit}
           />

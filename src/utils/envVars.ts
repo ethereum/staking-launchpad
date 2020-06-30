e383de5bad37afe34f358151630cdd7d7fdfdf79
George Weiler <georgejweiler@gmail.com>
2020-06-30 07:56:10 -0700

    Update src/utils/envVars.ts
    Fix explorer urls to point to the correct network.
    
    Co-authored-by: Carl Beekhuizen <carl@ethereum.org>

--- src/utils/envVars.ts
+++ src/utils/envVars.ts
@@ -7,8 +7,8 @@ export const ENABLE_RPC_FEATURES        = Boolean(INFURA_PROJECT_ID && INFURA_PR
 export const INFURA_URL                 = `https://${IS_MAINNET ? "mainnet" : "goerli"}.infura.io/v3/${INFURA_PROJECT_ID}`;
 
 // public
-export const ALETHIO_URL                = IS_MAINNET ? 'https://explorer.goerli.aleth.io/tx' : 'https://explorer.goerli.aleth.io/tx';
-export const ETHERSCAN_URL              = IS_MAINNET ? 'https://goerli.etherscan.io/tx' : 'https://goerli.etherscan.io/tx';
+export const ALETHIO_URL                = IS_MAINNET ? 'https://explorer.aleth.io/tx' : 'https://explorer.goerli.aleth.io/tx';
+export const ETHERSCAN_URL              = IS_MAINNET ? 'https://etherscan.io/tx' : 'https://goerli.etherscan.io/tx';
 export const FORTMATIC_KEY              = process.env.REACT_APP_FORTMATIC_KEY       || 'pk_test_D113D979E0D3508F';
 export const CONTRACT_ADDRESS           = process.env.REACT_APP_CONTRACT_ADDRESS    || '0x344b3a521ded954b4fa9ec8cc1d999631c998daa';
 export const ETH2_NETWORK_NAME          = process.env.REACT_APP_ETH2_NETWORK_NAME   || '';

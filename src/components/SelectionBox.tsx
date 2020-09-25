d9f26c78e83fbb74d6b7101fd02a5ffe90abcd65
Thomas Lipari <thomas.lipari@gmail.com>
2020-09-25 11:55:01 -0700

    make SelectionBox reusable

---
+++ src/components/SelectionBox.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const LogoContainerBox = styled.div`
+  width: 200px;
+  height: 200px;
+  border: ${(p: { theme: any; isActive: boolean }) =>
+    `2px solid ${p.isActive ? p.theme.blue.light : p.theme.gray.medium}`};
+  box-shadow: ${(p: { theme: any; isActive: boolean }) =>
+    p.isActive && `0 0 10px rgba(0, 0, 0, 0.5)`};
+  border-radius: ${p => p.theme.borderRadius};
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  cursor: pointer;
+  -webkit-transition: border 500ms ease-out, box-shadow 500ms ease-out;
+  -moz-transition: border 500ms ease-out, box-shadow 500ms ease-out;
+  -o-transition: border 500ms ease-out, box-shadow 500ms ease-out;
+  transition: border 500ms ease-out, box-shadow 500ms ease-out;
+  @media only screen and (max-width: 770px) {
+    width: 150px;
+    height: 150px;
+    margin: 17px;
+  }
+`;
+
+interface SelectionBoxProps {
+  isActive: boolean;
+  onClick: any;
+  children?: React.ReactElement | React.ReactElement[];
+  fullWidth?: boolean;
+  style?: any;
+}
+
+export const SelectionBox = ({
+  isActive,
+  onClick,
+  style,
+  children,
+}: SelectionBoxProps) => {
+  return (
+    <LogoContainerBox onClick={onClick} isActive={isActive} style={style}>
+      {children}
+    </LogoContainerBox>
+  );
+};

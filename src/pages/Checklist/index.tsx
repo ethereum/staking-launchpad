e797b045395703baba39c875e191d7df2cf0f667
George Weiler <georgejweiler@gmail.com>
2020-10-13 14:20:03 -0700

    complete the checklist page

--- src/pages/Checklist/index.tsx
+++ src/pages/Checklist/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styled from 'styled-components';
-// import { Link } from '../../components/Link';
+import { Link } from '../../components/Link';
 import { PageTemplate } from '../../components/PageTemplate';
 import { Heading } from '../../components/Heading';
 import { Text } from '../../components/Text';
+import { CheckBox } from 'grommet';
+import { ETH2_NETWORK_NAME, IS_MAINNET } from '../../utils/envVars';
 
 const ChecklistPageStyles = styled.div`
   section {
@@ -16,6 +18,13 @@ const ChecklistPageStyles = styled.div`
       margin-bottom: 5px;
     }
   }
+  label {
+    padding: 1rem;
+  }
+  .sub-checklist-item {
+    margin-top: -0.5rem;
+    margin-left: 1rem;
+  }
 `;
 
 export const Checklist = () => {
@@ -26,139 +35,256 @@ export const Checklist = () => {
           <Heading level={3}>Hardware</Heading>
           <Heading level={4}>Recommended</Heading>
           <ul>
-            <li>
-              We recommend you run the Validator Client (VC) on a separate
-              machine from your Beacon Node (BN). This is to ensure your
-              keystore(s) and password(s) are as secure as possible. Read here
-              to understand more about the architecture.
+            <li className="py5">
+              <Text>
+                We recommend you run the Validator Client (VC) on a separate
+                machine from your Beacon Node (BN). This is to ensure your
+                keystore(s) and password(s) are as secure as possible. Read{' '}
+                <Link
+                  primary
+                  external
+                  inline
+                  to="https://blog.ethereum.org/2019/11/27/validated-staking-on-eth2-0/"
+                >
+                  here
+                </Link>{' '}
+                to understand more about the architecture.
+              </Text>
             </li>
-            <li>
-              In order to process incoming validator deposits from the eth1
-              chain, you'll need to run an eth1 client in parallel to your eth2
-              client. While it is possible to use a third-party service like
-              Infura, we recommend running your own client in order to ensure
-              the network stays as decentralised as possible.
+            <li className="py5">
+              <Text>
+                In order to process incoming validator deposits from the eth1
+                chain, you'll need to run an eth1 client in parallel to your
+                eth2 client. While it is possible to use a third-party service
+                like Infura, we recommend running your own client in order to
+                ensure the network stays as decentralised as possible.
+              </Text>
             </li>
             <ul>
               <li>
-                For example, you need at least 140 GB SSD to run geth fast sync
-                on mainnet.
+                <Text>
+                  For example, you need at least 140 GB SSD to run geth fast
+                  sync on <i>mainnet</i>.
+                </Text>
               </li>
             </ul>
           </ul>
         </section>
         <section>
           <Heading level={3}>Keystore</Heading>
-          <ol>
-            <li>I have securely generated and saved my keystore(s).</li>
-            <li>I have backed up my mnemonic.</li>
-            <li>I have set a strong password for my keystore(s)</li>
-            <li>I have backed up my password.</li>
-          </ol>
+          <CheckBox
+            label={
+              <Text>I have securely generated and saved my keystore(s).</Text>
+            }
+          />
+          <CheckBox label={<Text>I have backed up my mnemonic.</Text>} />
+          <CheckBox
+            label={<Text>I have set a strong password for my keystore(s)</Text>}
+          />
+          <CheckBox label={<Text>I have backed up my password.</Text>} />
         </section>
         <section>
           <Heading level={3}>Security</Heading>
-          <ol>
-            <li> I have set up a firewall on my machine(s).</li>
-          </ol>
+          <CheckBox
+            label={<Text>I have set up a firewall on my machine(s). </Text>}
+          />
         </section>
         <section>
           <Heading level={3}>Eth1 Client</Heading>
-          <ol>
-            <li>
-              I have installed and synced my Eth1 node on ETH1_NETWORK_NAME.
-            </li>
-          </ol>
+          <CheckBox
+            label={
+              <Text>
+                I have installed and synced my Eth1 node on{' '}
+                {IS_MAINNET ? 'mainnet' : 'goerli'}.
+              </Text>
+            }
+          />
         </section>
         <section>
           <Heading level={3}>Eth2 Beacon Node (BN)</Heading>
           <Heading level={4}>Required</Heading>
-          <ol>
+          <CheckBox
+            label={
+              <Text>
+                I have installed the{' '}
+                <strong>latest stable software release</strong> of my client.
+              </Text>
+            }
+          />
+          <ul className="sub-checklist-item">
             <li>
-              I have installed the latest stable software release of my client.
-              <ul>
-                <li>
-                  Note: If you're setting up your client before phase 0 launch,
-                  it is your responsibility to check for any new software
-                  releases in the run up to launch. There is a good chance you
-                  will need to update your software.
-                </li>
-              </ul>
+              <Text>
+                <strong>Note:</strong> If you're setting up your client before
+                phase 0 launch, it is your responsibility to check for any new
+                software releases in the run up to launch. There is a good
+                chance you will need to update your software.
+              </Text>
             </li>
+          </ul>
+          <CheckBox
+            label={
+              <Text>
+                My Eth2 beacon node is able to connect to my Eth1 client via
+                HTTP API(s).
+              </Text>
+            }
+          />
+          <CheckBox
+            label={
+              <Text>
+                I have synced my Eth2 beacon node on {ETH2_NETWORK_NAME}.
+              </Text>
+            }
+          />
+          <ul className="sub-checklist-item">
             <li>
-              My Eth2 beacon node is able to connect to my Eth1 client via HTTP
-              API(s).
+              <Text>
+                <strong>Note:</strong> Please check that your node has greater
+                than 20 peers.
+              </Text>
             </li>
-            <li>
-              I have synced my Eth2 beacon node on ETH2_NETWORK_NAME.
-              <ul>
-                <li>
-                  Note: Please check that your node has greater than 20 peers.
-                </li>
-              </ul>
-            </li>
-          </ol>
+          </ul>
           <Heading level={4}>Recommended</Heading>
-
-          <ol>
+          <CheckBox
+            label={<Text>I have joined my client's discord chat.</Text>}
+          />
+          <ul className="sub-checklist-item">
             <li>
-              I have joined my client's discord chat.
-              <ul>
-                <li>Lighthouse | Nimbus | Prysm | Teku</li>
-              </ul>
+              <Link primary inline external to="https://discord.gg/uC7TuaH">
+                Lighthouse
+              </Link>{' '}
+              |{' '}
+              <Link primary inline external to="https://discord.gg/YbTCNat">
+                Nimbus
+              </Link>{' '}
+              |{' '}
+              <Link primary inline external to="https://discord.gg/KSA7rPr">
+                Prysm
+              </Link>{' '}
+              |{' '}
+              <Link primary inline external to="Teku">
+                Teku
+              </Link>
             </li>
-          </ol>
-          <ol>
-            <li> I have opened the ports on my router.</li>
-          </ol>
+          </ul>
+          <CheckBox
+            label={<Text>I have opened the ports on my router.</Text>}
+          />
         </section>
         <section>
           <Heading level={3}>Eth2 Validator Client (VC)</Heading>
           <Heading level={4}>Required</Heading>
-          <ol>
-            <li>
-              I have imported my keystore(s) into my Eth2 validator client.
-            </li>
-            <li> I have started running my validator client.</li>
-          </ol>
+          <CheckBox
+            label={
+              <Text>
+                I have imported my keystore(s) into my Eth2 validator client.
+              </Text>
+            }
+          />
+          <CheckBox
+            label={<Text>I have started running my validator client.</Text>}
+          />
           <Heading level={4}>Recommended</Heading>
-          <ol>
-            <li>
-              I have set up my validator client on a separate machine and
-              connected it to my Beacon Node via HTTP/gRPC API(s).
-            </li>
-          </ol>
+          <CheckBox
+            label={
+              <Text>
+                I have set up my validator client on a separate machine and
+                connected it to my Beacon Node via HTTP/gRPC API(s).
+              </Text>
+            }
+          />
         </section>
-
         <section>
           <Heading level={3}>[Optional] Prometheus + Grafana monitor</Heading>
           <Text>
             The Eth2 clients support Prometheus + Grafana to help you visualise
             important real-time metrics concerning your validator. You can find
-            client specific instructions here:
+            client specific instructions here:{' '}
+            <Link
+              primary
+              inline
+              external
+              to="https://github.com/sigp/lighthouse-metrics"
+            >
+              Lighthouse
+            </Link>{' '}
+            |{' '}
+            <Link
+              primary
+              inline
+              external
+              to="https://status-im.github.io/nim-beacon-chain/metrics-pretty-pictures.html"
+            >
+              Nimbus
+            </Link>{' '}
+            |{' '}
+            <Link
+              primary
+              inline
+              external
+              to="https://docs.prylabs.network/docs/prysm-usage/monitoring/grafana-dashboard/"
+            >
+              Prysm
+            </Link>{' '}
+            |{' '}
+            <Link
+              primary
+              inline
+              external
+              to="https://docs.teku.consensys.net/en/latest/HowTo/Monitor/Metrics/"
+            >
+              Teku
+            </Link>
           </Text>
-          <ul>
-            <li>Lighthouse | Nimbus | Prysm | Teku</li>
-          </ul>
-          <ol>
-            <li>I have set up my Prometheus service.</li>
-            <li>I have set up my Grafana service.</li>
-            <li>
-              I have imported the dashboard config to my Grafana server and
-              double checked that my node is alive.
-            </li>
-          </ol>
+          <CheckBox
+            label={
+              <Text>
+                I have set up my{' '}
+                <Link primary inline external to="https://prometheus.io/">
+                  Prometheus
+                </Link>{' '}
+                service.
+              </Text>
+            }
+          />
+          <CheckBox
+            label={
+              <Text>
+                I have set up my{' '}
+                <Link primary inline external to="https://grafana.com/">
+                  Grafana
+                </Link>{' '}
+                service.
+              </Text>
+            }
+          />
+          <CheckBox
+            label={
+              <Text>
+                I have imported the dashboard config to my Grafana server and
+                double checked that my node is alive.
+              </Text>
+            }
+          />
         </section>
         <section>
-          <Heading level={3}>[Optional] POAP Graffiti</Heading>
+          <Heading level={3}>
+            [Optional]{' '}
+            <Link primary inline external to="https://beaconcha.in/poap">
+              POAP
+            </Link>{' '}
+            Graffiti
+          </Heading>
           <Text>
             You can use your validator client's graffiti flag to add a personal
             touch to your proposed blocks (some text of your choice). You'll be
-            able to see it using the block explorer.
+            able to see it using the{' '}
+            <Link primary external inline to="block explorer">
+              block explorer
+            </Link>
+            .
           </Text>
-          <ol>
-            <li> I have set my graffiti flag.</li>
-          </ol>
+          <CheckBox label={<Text>I have set my graffiti flag.</Text>} />
         </section>
       </ChecklistPageStyles>
     </PageTemplate>

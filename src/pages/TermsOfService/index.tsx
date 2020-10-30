d61324eeca524d948c5df3dcdce00a88fa210ff7
George Weiler <georgejweiler@gmail.com>
2020-10-30 13:46:49 -0700

    add more placeholder text

--- src/pages/TermsOfService/index.tsx
+++ src/pages/TermsOfService/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { AppBar } from '../../components/AppBar';
-import { Link } from '../../components/Link';
+// import { Link } from '../../components/Link';
 import { Heading } from '../../components/Heading';
 import { Text } from '../../components/Text';
 
@@ -55,18 +55,102 @@ export const TermsOfService = () => {
       <AppBar />
       <Container>
         <TermsOfServiceStyles>
+          <Text
+            size="12px"
+            color="grayDark"
+            className="tos-text last-updated-text"
+          >
+            Page last updated: October 30, 2020
+          </Text>
+          <Heading level={2}>Terms of Use</Heading>
+          <Text className="section-title tos-text" weight={500}>
+            PLEASE READ THESE TERMS OF USE BEFORE USING THE WEBSITE.
+          </Text>
           <section>
-            <Text
-              size="12px"
-              color="grayDark"
-              className="tos-text last-updated-text"
-            >
-              Page last updated: June 24, 2020
+            <Text className="tos-text">
+              Acceptance of the Terms of Use These terms of use are entered into
+              by and between you and the Ethereum Foundation ("Foundation", "we"
+              or "us"). The following terms and conditions, together with any
+              documents they expressly incorporate by reference (collectively,
+              these "Terms of Use"), govern your access to and use of
+              ethereum.org, including any content, functionality and services
+              offered on or through ethereum.org, ethereum.foundation,
+              devcon.org, and blog.ethereum.org (together, the "Website").
             </Text>
-            <Heading level={2}>Terms of Use</Heading>
+            <Text className="tos-text">
+              Please read the Terms of Use carefully before you start to use the
+              Websites. By using the Websites or by clicking to accept or agree
+              to the Terms of Use when this option is made available to you, you
+              accept and agree to be bound and abide by these Terms of Use in
+              addition to
+              <ul>
+                <li>
+                  our Privacy Policy, incorporated herein by reference; and
+                </li>
+                <li> our Cookie Policy, incorporated herein by reference.</li>
+              </ul>
+            </Text>
+            <Text className="tos-text">
+              If you do not to agree to these Terms of Use or the Privacy
+              Policy, you must not access or use the Websites.
+            </Text>
             <Text className="section-title tos-text" weight={500}>
-              PLEASE READ THESE TERMS OF USE BEFORE USING THE WEBSITE.
+              Who May Use the Websites
             </Text>
+
+            <Text className="tos-text">
+              This Websites is offered and available to users who are 13 years
+              of age or older. The Websites is not intended for children under
+              13 years of age. By using this Website, you represent and warrant
+              that you (i) are 13 years of age or older, (ii) are not barred to
+              use the Websites under any applicable law, and (iii) are using the
+              Websites only for your own personal use. If you do not meet these
+              requirements, you must not access or use the Websites.
+            </Text>
+          </section>
+          <section>
+            <Text className="tos-text">
+              Acceptance of the Terms of Use These terms of use are entered into
+              by and between you and the Ethereum Foundation ("Foundation", "we"
+              or "us"). The following terms and conditions, together with any
+              documents they expressly incorporate by reference (collectively,
+              these "Terms of Use"), govern your access to and use of
+              ethereum.org, including any content, functionality and services
+              offered on or through ethereum.org, ethereum.foundation,
+              devcon.org, and blog.ethereum.org (together, the "Website").
+            </Text>
+            <Text className="tos-text">
+              Please read the Terms of Use carefully before you start to use the
+              Websites. By using the Websites or by clicking to accept or agree
+              to the Terms of Use when this option is made available to you, you
+              accept and agree to be bound and abide by these Terms of Use in
+              addition to
+              <ul>
+                <li>
+                  our Privacy Policy, incorporated herein by reference; and
+                </li>
+                <li> our Cookie Policy, incorporated herein by reference.</li>
+              </ul>
+            </Text>
+            <Text className="tos-text">
+              If you do not to agree to these Terms of Use or the Privacy
+              Policy, you must not access or use the Websites.
+            </Text>
+            <Text className="section-title tos-text" weight={500}>
+              Who May Use the Websites
+            </Text>
+
+            <Text className="tos-text">
+              This Websites is offered and available to users who are 13 years
+              of age or older. The Websites is not intended for children under
+              13 years of age. By using this Website, you represent and warrant
+              that you (i) are 13 years of age or older, (ii) are not barred to
+              use the Websites under any applicable law, and (iii) are using the
+              Websites only for your own personal use. If you do not meet these
+              requirements, you must not access or use the Websites.
+            </Text>
+          </section>
+          <section>
             <Text className="tos-text">
               Acceptance of the Terms of Use These terms of use are entered into
               by and between you and the Ethereum Foundation ("Foundation", "we"

c34f7a3025f75e9d2f5d6d7722f67861d3f5555d
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2021-02-01 21:48:41 -0800

    adds translation component and utilities

---
+++ src/components/Translation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+import { getDefaultMessage } from '../utils/translations';
+
+// Wrapper on <FormattedMessage /> to always fallback to English
+// Use this component for any user-facing string
+const Translation = (props: any) => (
+  <FormattedMessage
+    id={props.id}
+    defaultMessage={getDefaultMessage(props.id)}
+  />
+);
+
+export default Translation;
---
+++ src/utils/translations.ts
@@ -0,0 +1,39 @@
+const defaultStrings = require('../intl/en.json');
+
+// Returns the en.json value
+export const getDefaultMessage = (key: string) => {
+  const defaultMessage = defaultStrings[key];
+  if (defaultMessage === undefined) {
+    console.error(
+      `No key "${key}" in en.json. Cannot provide a default message.`
+    );
+  }
+  return defaultMessage || '';
+};
+
+export const isLangRightToLeft = (lang: string): boolean => {
+  // Todo: Revisit when language scope decided
+  return lang === 'ar' || lang === 'fa';
+};
+
+export const translateMessageId = (id: string, intl: any) => {
+  if (!id) {
+    console.error(`No id provided for translation.`);
+    return '';
+  }
+  if (!intl || !intl.formatMessage) {
+    console.error(`Invalid/no intl provided for translation id ${id}`);
+    return '';
+  }
+  const translation = intl.formatMessage({
+    id,
+    defaultMessage: getDefaultMessage(id),
+  });
+  if (translation === id) {
+    console.error(
+      `Intl ID string "${id}" has no match. Default message of "" returned.`
+    );
+    return '';
+  }
+  return translation;
+};

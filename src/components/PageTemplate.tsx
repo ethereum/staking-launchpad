f69adff66c6fbb5d5f1026d359cba96ef03abcab
Sam Richards <sbrichards@gmail.com>
2021-03-16 07:39:57 -0700

    Add Twitter meta tags [Fixes #373]

--- src/components/PageTemplate.tsx
+++ src/components/PageTemplate.tsx
@@ -44,8 +44,10 @@ const _PageTemplate = ({
       <Helmet>
         <title>{title}</title>
         <meta property="og:title" content={title} />
+        <meta property="twitter:title" content={title} />
         <meta name="description" content={description} />
         <meta property="og:description" content={description} />
+        <meta property="twitter:description" content={description} />
       </Helmet>
       <AppBar />
       <Gutter>

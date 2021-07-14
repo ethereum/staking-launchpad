d16e10c9b6f58cd3804870d68af136586b3f5dad
Sam Richards <sbrichards@gmail.com>
2021-07-14 13:25:44 -0700

    Update sort to use localeCompare

--- src/pages/Languages/index.tsx
+++ src/pages/Languages/index.tsx
@@ -144,11 +144,7 @@ export const Languages = () => {
     },
   ]
     .filter(lang => supportedLanguages.includes(lang.code))
-    .sort((a, b) => {
-      if (a.title < b.title) return -1;
-      if (a.title > b.title) return 1;
-      return 0;
-    });
+    .sort((a, b) => a.title.localeCompare(b.title));
 
   return (
     <PageTemplate

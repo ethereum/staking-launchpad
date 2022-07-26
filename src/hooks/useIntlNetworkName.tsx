7206373c5588cccc224d9a08853e17fd5285a0a7
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2022-07-27 00:07:03 +0200

    Create useIntlNetworkName.tsx

---
+++ src/hooks/useIntlNetworkName.tsx
@@ -0,0 +1,28 @@
+import { useIntl } from 'react-intl';
+import {
+  IS_MAINNET,
+  EL_TESTNET_NAME,
+  TESTNET_LAUNCHPAD_NAME,
+} from '../utils/envVars';
+
+const useNetworkName = (): {
+  executionLayerName: string;
+  consensusLayerName: string;
+} => {
+  const { formatMessage } = useIntl();
+  const executionLayerName: string = IS_MAINNET
+    ? formatMessage({ defaultMessage: 'Mainnet' })
+    : formatMessage(
+        { defaultMessage: '{EL_TESTNET_NAME} testnet' },
+        { EL_TESTNET_NAME }
+      );
+  const consensusLayerName: string = IS_MAINNET
+    ? formatMessage({ defaultMessage: 'Mainnet' })
+    : formatMessage(
+        { defaultMessage: '{TESTNET_LAUNCHPAD_NAME} testnet' },
+        { TESTNET_LAUNCHPAD_NAME }
+      );
+  return { executionLayerName, consensusLayerName };
+};
+
+export default useNetworkName;

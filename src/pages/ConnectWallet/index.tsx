e20304be043a226282dc6ce4d7df8afdc368a8c9
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-01-18 12:44:29 -0800

    add check for 0x01 before depositing
    Shows chosen withdrawal address on summary before depositing, and issues a warning if user has not provided a withdrawal address.

--- src/pages/ConnectWallet/index.tsx
+++ src/pages/ConnectWallet/index.tsx
@@ -21,6 +21,7 @@ import {
   useMetamaskEagerConnect,
   useMetamaskListener,
 } from './web3Utils';
+import { Alert } from '../../components/Alert';
 import { Button } from '../../components/Button';
 import { WorkflowPageTemplate } from '../../components/WorkflowPage/WorkflowPageTemplate';
 import { DepositKeyInterface, StoreState } from '../../store/reducers';
@@ -313,6 +314,17 @@ const _ConnectWalletPage = ({
     executionLayerName,
   ]);
 
+  const withdrawalAddress = useMemo<string>(() => {
+    // eslint-disable-next-line camelcase
+    const credentials = depositKeys[0]?.withdrawal_credentials ?? '';
+    if (credentials.startsWith('01')) return `0x${credentials.slice(-40)}`;
+    return '';
+  }, [depositKeys]);
+  const withdrawalAddressShort = useMemo<string>(
+    () => `${withdrawalAddress.slice(0, 6)}...${withdrawalAddress.slice(-4)}`,
+    [withdrawalAddress]
+  );
+
   const handleSubmit = () => {
     if (workflow === WorkflowStep.CONNECT_WALLET) {
       dispatchWorkflowUpdate(WorkflowStep.SUMMARY);
@@ -422,6 +434,30 @@ const _ConnectWalletPage = ({
                       </FaucetLink>
                     )}
                   </div>
+                  <Alert
+                    variant={withdrawalAddress ? 'warning' : 'error'}
+                    className="mt20"
+                  >
+                    {withdrawalAddress ? (
+                      <span>
+                        The withdrawal address for these validators will be set
+                        to{' '}
+                        <span title={withdrawalAddress}>
+                          {withdrawalAddressShort}
+                        </span>
+                        . Make 100% sure you control this address before
+                        depositing, as this cannot be changed.
+                      </span>
+                    ) : (
+                      <span>
+                        A withdrawal address has not been set for these
+                        validators. It is recommended to do this while
+                        generating your deposit data, otherwise you will have
+                        additional steps to enable withdrawals once the Shanghai
+                        upgrade is live.
+                      </span>
+                    )}
+                  </Alert>
                 </>
               )}
             </Paper>

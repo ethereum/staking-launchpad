31b686fff623f317380e801b08c8c0a2f03179e6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-01-26 19:08:41 -0800

    Create WithdrawalCredentials.tsx

---
+++ src/components/WithdrawalCredentials.tsx
@@ -0,0 +1,153 @@
+// Import libraries
+import React, { FC, useState, useMemo, ChangeEvent } from 'react';
+import styled from 'styled-components';
+import { FormattedMessage, useIntl } from 'react-intl';
+// Components
+import { Alert } from './Alert';
+import { Button } from './Button';
+import { Spinner } from './Spinner';
+// Constants
+import { BEACONCHAIN_URL, IS_MAINNET, NETWORK_NAME } from '../utils/envVars';
+import { screenSizes } from '../styles/styledComponentsTheme';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  font-size: 1.25rem;
+  padding: 15px;
+  width: 20ch;
+  @media (max-width: ${screenSizes.smaller}) {
+    width: 100%;
+  }
+`;
+
+interface Validator {
+  validatorIndex: number;
+  withdrawalCredentials: string;
+  isUpgraded: boolean;
+}
+
+interface IProps {}
+export const WithdrawalCredentials: FC<IProps> = () => {
+  const { locale } = useIntl();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [hasError, setHasError] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>('');
+  const [validator, setValidator] = useState<Validator | null>(null);
+
+  const checkWithdrawalCredentials = async () => {
+    setHasError(false);
+    setIsLoading(true);
+    const endpoint = `${BEACONCHAIN_URL}/api/v1/validator/${inputValue}`;
+    try {
+      const response = await fetch(endpoint);
+      const { data } = await response.json();
+      const withdrawalCredentials = data.withdrawalcredentials;
+      setValidator({
+        validatorIndex: parseInt(inputValue, 10),
+        withdrawalCredentials,
+        isUpgraded: withdrawalCredentials.startsWith('0x01'),
+      });
+    } catch (error) {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setInputValue(e.target.value.replace(/\D/g, ''));
+
+  const longAddress = useMemo<string>(
+    () => (validator ? `0x${validator.withdrawalCredentials.slice(-40)}` : ''),
+    [validator]
+  );
+  const shortAddress = useMemo<string>(
+    () =>
+      longAddress ? `${longAddress.slice(0, 6)}…${longAddress.slice(-4)}` : '',
+    [longAddress]
+  );
+  const resultText = useMemo<string | JSX.Element>(() => {
+    if (isLoading) return <Spinner />;
+    if (hasError)
+      return (
+        <Alert variant="error">
+          <FormattedMessage defaultMessage="Oops! Double check validator index number and try again." />
+        </Alert>
+      );
+    if (!validator) return ' ';
+    if (validator.isUpgraded)
+      return (
+        <Alert variant="warning">
+          <FormattedMessage
+            defaultMessage="Validator index {validatorIndex} is ready to start
+            receiving rewards! Withdrawal credentials linked to execution address {address}"
+            values={{
+              validatorIndex: (
+                <strong>
+                  {new Intl.NumberFormat(locale).format(
+                    validator.validatorIndex
+                  )}
+                </strong>
+              ),
+              address: <strong title={longAddress}>{shortAddress}</strong>,
+            }}
+          />
+        </Alert>
+      );
+    return (
+      <Alert variant="warning">
+        <FormattedMessage
+          defaultMessage="This {network} validator needs to be upgraded."
+          values={{
+            network: IS_MAINNET ? (
+              ''
+            ) : (
+              <FormattedMessage
+                defaultMessage="{NETWORK_NAME} testnet"
+                values={{ NETWORK_NAME }}
+              />
+            ),
+          }}
+        />
+      </Alert>
+    );
+  }, [isLoading, hasError, validator, longAddress, shortAddress]);
+
+  return (
+    <Container>
+      <FlexRow>
+        <Input
+          id="validatorIndex"
+          value={inputValue}
+          onChange={handleChange}
+          placeholder="Validator index"
+        />
+        <Button
+          label={
+            <FormattedMessage
+              defaultMessage="Check {NETWORK_NAME}"
+              values={{ NETWORK_NAME }}
+            />
+          }
+          // rainbow
+          onClick={checkWithdrawalCredentials}
+          disabled={!inputValue.length}
+        />
+      </FlexRow>
+      <section>{resultText}</section>
+    </Container>
+  );
+};

3c2fde486517363ed3e111bbb5db72514d3153ba
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-04-04 14:17:20 -0700

    refactor Code component
    Moves hard-coded LTR direction assignment to an inner-span that only styles the text itself, allowing the surrounding span to be styled with direction-responsive margins and padding.

--- src/components/Code.tsx
+++ src/components/Code.tsx
@@ -1,14 +1,28 @@
-import styled from 'styled-components';
+import React, { FC } from 'react';
+import styled, { StyledProps } from 'styled-components';
 import { Text } from './Text';
 
-export const Code = styled(Text)`
+const CodeContainer = styled(Text)`
   border: 1px solid #dc8180;
   background-color: ${(p: any) => p.theme.red.lightest};
   display: inline-block;
   border-radius: 4px;
   padding: 0 4px;
+  direction: inherit;
+`;
+
+const CodeText = styled(Text)`
   color: #ad2b2a;
   font-size: 14px;
   line-height: 16px;
-  direction: ltr;
+  direction: ltr; /* Hard-coded LTR for English code snippet text */
 `;
+
+interface CodeProps extends StyledProps<any> {
+  className?: string;
+}
+export const Code: FC<CodeProps> = ({ children, className }) => (
+  <CodeContainer className={className}>
+    <CodeText>{children}</CodeText>
+  </CodeContainer>
+);

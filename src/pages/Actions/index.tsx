20105744ed65fe9966046fdf86e3074f82e05e1f
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-01-30 14:44:37 -0800

    refactor: use latest component updates

--- src/pages/Actions/index.tsx
+++ src/pages/Actions/index.tsx
@@ -19,6 +19,7 @@ import { Alert } from '../../components/Alert';
 import Select from '../../components/Select';
 import Spinner from '../../components/Spinner';
 import { Text } from '../../components/Text';
+import ValidatorDetails from './components/ValidatorDetails';
 
 import { web3ReactInterface } from '../ConnectWallet';
 import { AllowedELNetworks, NetworkChainId } from '../ConnectWallet/web3Utils';
@@ -30,11 +31,13 @@ import {
   TICKER_NAME,
 } from '../../utils/envVars';
 
-const Container = styled.div`
+const Section = styled.section`
   background-color: white;
   padding: 1rem;
-  margin: 1rem;
   border-radius: 4px;
+  &:not(:last-child) {
+    margin-bottom: 1rem;
+  }
 `;
 
 const FakeLink = styled.span`
@@ -62,6 +65,8 @@ const ButtonContainer = styled.div`
     }
   }
 `;
+
+const MAX_QUERY_LIMIT = 15;
 
 const _ActionsPage: React.FC<Props> = () => {
   const {
@@ -94,7 +99,7 @@ const _ActionsPage: React.FC<Props> = () => {
       // beaconchain API requires two fetches - one that gets the public keys for an Ethereum address, and one that
       // queries by the validators public keys
       fetch(
-        `${BEACONCHAIN_URL}/api/v1/validator/withdrawalCredentials/${account}?limit=100`
+        `${BEACONCHAIN_URL}/api/v1/validator/withdrawalCredentials/${account}?limit=${MAX_QUERY_LIMIT}`
       )
         .then(r => r.json())
         .then(
@@ -117,7 +122,7 @@ const _ActionsPage: React.FC<Props> = () => {
             } else {
               // query by public keys
               const pubKeysCommaDelineated = `${response
-                .slice(0, 50)
+                .slice(0, MAX_QUERY_LIMIT)
                 .map(validator => validator.publickey)
                 .join(',')}`;
 
@@ -179,7 +184,7 @@ const _ActionsPage: React.FC<Props> = () => {
 
     if (!active) {
       return (
-        <Container>
+        <Section>
           <div className="mt20">
             <Text className="mt10">
               <FormattedMessage defaultMessage="Connect your wallet to retrieve validators with corresponding withdrawal credentials to upgrade to compounding, consolidate, partially withdraw, or force an exit" />
@@ -193,7 +198,7 @@ const _ActionsPage: React.FC<Props> = () => {
               />
             </ButtonLink>
           </ButtonContainer>
-        </Container>
+        </Section>
       );
     }
 
@@ -218,58 +223,69 @@ const _ActionsPage: React.FC<Props> = () => {
 
     return (
       <>
-        <Container>
+        <Section>
           <Text>
             <FormattedMessage defaultMessage="Select a validator" />
           </Text>
-          <Select
-            placeholder={`Total validators: ${validators.length}`}
-            searchPlaceholder={formatMessage({
-              defaultMessage: 'Filter by index or pubkey',
-            })}
-            options={validators.map(v => ({
-              value: v.pubkey,
-              searchContext: `${v.validatorindex.toString()}:${v.pubkey}`,
-              label: (
-                <div
-                  style={{
-                    display: 'grid',
-                    gridTemplateColumns: '1fr auto',
-                    flex: 1,
-                    gap: '1rem',
-                    width: '100%',
-                  }}
-                >
-                  <p style={{ margin: 0, textAlign: 'start' }}>
-                    {v.validatorindex.toString()}
-                  </p>
-                  <p
+
+          <div style={{ display: 'flex', gap: '1rem' }}>
+            <Select
+              placeholder={`Total validators: ${validators.length}`}
+              searchPlaceholder={formatMessage({
+                defaultMessage: 'Filter by index or pubkey',
+              })}
+              options={validators.map(v => ({
+                value: v.pubkey,
+                searchContext: `${v.validatorindex.toString()}:${v.pubkey}`,
+                label: (
+                  <div
                     style={{
-                      margin: 0,
-                      textAlign: 'end',
-                      marginInlineStart: 'auto',
-                      fontSize: '0.75em',
-                      color: '#444',
+                      display: 'grid',
+                      gridTemplateColumns: '1fr auto',
+                      flex: 1,
+                      gap: '1rem',
+                      width: '100%',
                     }}
                   >
-                    {(v.balance / ETHER_TO_GWEI).toFixed(9)} {TICKER_NAME}
-                  </p>
-                </div>
-              ),
-            }))}
-            value={selectedValidator?.pubkey || ''}
-            onChange={(value: string) => {
-              const validator = validators.find(v => v.pubkey === value);
-              setSelectedValidator(validator || null);
-            }}
-          />
-        </Container>
+                    <p style={{ margin: 0, textAlign: 'start' }}>
+                      {v.validatorindex.toString()}
+                    </p>
+                    <p
+                      style={{
+                        margin: 0,
+                        textAlign: 'end',
+                        marginInlineStart: 'auto',
+                        fontSize: '0.75em',
+                        color: '#444',
+                      }}
+                    >
+                      {(v.balance / ETHER_TO_GWEI).toFixed(9)} {TICKER_NAME}
+                    </p>
+                  </div>
+                ),
+              }))}
+              value={selectedValidator?.pubkey || ''}
+              onChange={(value: string) => {
+                const validator = validators.find(v => v.pubkey === value);
+                setSelectedValidator(validator || null);
+              }}
+            />
+            <Button
+              label={<FormattedMessage defaultMessage="Fetch more" />}
+              // onClick={() => {}} // TODO: implement fetch more logic
+            />
+          </div>
+        </Section>
 
         {selectedValidator && (
-          <ValidatorActions
-            validator={selectedValidator}
-            validators={validators}
-          />
+          <>
+            <ValidatorDetails validator={selectedValidator} />
+
+            <ValidatorActions
+              validator={selectedValidator}
+              validators={validators}
+            />
+          </>
         )}
       </>
     );

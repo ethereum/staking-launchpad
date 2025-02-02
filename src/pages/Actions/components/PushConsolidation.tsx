460a92868da77596712355f009a251cc3a8bf018
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2025-02-01 16:28:29 -0800

    feat: update PushConsolidation modal flow

--- src/pages/Actions/components/PushConsolidation.tsx
+++ src/pages/Actions/components/PushConsolidation.tsx
@@ -1,33 +1,43 @@
+import React, { useState } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
+import { Box, Layer } from 'grommet';
+import { Alert as AlertIcon } from 'grommet-icons';
+import Web3 from 'web3';
 import { AbstractConnector } from '@web3-react/abstract-connector';
 import { useWeb3React } from '@web3-react/core';
-import { Box, Heading, Layer } from 'grommet';
-import React, { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
-import Web3 from 'web3';
+
 import { Validator } from '../types';
+
+import { Alert } from '../../../components/Alert';
+import { Button } from '../../../components/Button';
+import { Heading } from '../../../components/Heading';
+import { Text } from '../../../components/Text';
 import {
   TransactionStatus,
   TransactionStatusModal,
 } from '../../../components/TransactionStatusModal';
-import { generateCompoundParams } from '../ActionUtils';
-import { Button } from '../../../components/Button';
+import ModalHeader from './ModalHeader';
+import { ModalBody, AlertContent, ModalContent, Hash } from './Shared';
 import ValidatorSelector from './ValidatorSelector';
 
+import { generateCompoundParams } from '../ActionUtils';
+import { TICKER_NAME } from '../../../utils/envVars';
+
 type PushConsolidationProps = {
-  sourceValidator: Validator; // The selected validator to drain and migrate (source)
-  targetValidatorSet: Validator[]; // List of available target validators (Any 0x02 or higher, excluding the source)
+  sourceValidator: Validator; // The selected validator to consolidate into (target)
+  targetValidatorSet: Validator[]; // List of available source validators (Any 0x01 or higher, excluding the target)
 };
 
 const PushConsolidation = ({
   sourceValidator,
   targetValidatorSet,
 }: PushConsolidationProps) => {
+  const { locale } = useIntl();
   const { connector, account } = useWeb3React();
 
-  const [
-    selectedTargetValidator,
-    setSelectedTargetValidator,
-  ] = useState<Validator | null>(null);
+  const [selectedValidator, setSelectedValidator] = useState<Validator | null>(
+    null
+  );
   const [showSelectValidatorModal, setShowSelectValidatorModal] = useState<
     boolean
   >(false);
@@ -39,21 +49,18 @@ const PushConsolidation = ({
 
   const confirmConsolidate = () => {
     setShowSelectValidatorModal(true);
-    setSelectedTargetValidator(null);
+    setSelectedValidator(null);
   };
 
   const closeSelectValidatorModal = () => {
     setShowSelectValidatorModal(false);
-    setSelectedTargetValidator(null);
+    setSelectedValidator(null);
   };
 
   const createConsolidationTransaction = async () => {
-    if (!account || !selectedTargetValidator) {
-      return;
-    }
+    if (!account || !selectedValidator) return;
 
     setTransactionStatus('waiting_user_confirmation');
-    setShowSelectValidatorModal(false);
     setShowTxModal(true);
 
     try {
@@ -63,8 +70,8 @@ const PushConsolidation = ({
       const params = await generateCompoundParams(
         web3,
         account,
-        sourceValidator.pubkey,
-        selectedTargetValidator.pubkey
+        selectedValidator.pubkey,
+        sourceValidator.pubkey
       );
       return web3.eth
         .sendTransaction(params)
@@ -83,67 +90,243 @@ const PushConsolidation = ({
     }
   };
 
+  const handleClose = () => {
+    setShowTxModal(false);
+    closeSelectValidatorModal();
+  };
   return (
     <>
       {showSelectValidatorModal && (
-        <Layer position="center" onEsc={() => closeSelectValidatorModal()}>
-          <Box pad="medium" gap="small" width="medium">
-            <Heading level={3} margin="none">
-              <FormattedMessage
-                defaultMessage="Which validator would you like to migrate {index} funds to?"
-                values={{ index: sourceValidator.validatorindex }}
-              />
-            </Heading>
-            <ValidatorSelector
-              validators={targetValidatorSet}
-              selectedValidator={selectedTargetValidator}
-              setSelectedValidator={setSelectedTargetValidator}
-            />
+        <Layer
+          position="center"
+          onEsc={closeSelectValidatorModal}
+          onClickOutside={closeSelectValidatorModal}
+          style={{
+            background: '#EEEEEE',
+            width: 'clamp(min(432px,100%), 50vw, 40rem)',
+          }}
+        >
+          <Box>
+            <ModalHeader onClose={closeSelectValidatorModal}>
+              <FormattedMessage defaultMessage="Migrate and exit validator" />
+            </ModalHeader>
+            <ModalBody>
+              {!selectedValidator && (
+                <ModalContent>
+                  <Text>
+                    <FormattedMessage defaultMessage="This will initiate the process of permanently exiting the chosen validator from the Ethereum proof-of-stake network." />
+                  </Text>
+                  <Text>
+                    <FormattedMessage defaultMessage="You'll be asked to sign a message with your wallet. Processing of exits is not immediate, so account for several days before completion." />
+                  </Text>
+                  <ul style={{ paddingInlineStart: '1.5rem', marginTop: 0 }}>
+                    <li>
+                      <Text as="span">
+                        <FormattedMessage defaultMessage="Action is permanent and irreversible" />
+                      </Text>
+                    </li>
+                    <li>
+                      <Text as="span">
+                        <FormattedMessage defaultMessage="Account still responsible for consensus duties until exit epoch reached (keep validator online)" />
+                      </Text>
+                    </li>
+                    <li>
+                      <Text as="span">
+                        <FormattedMessage
+                          defaultMessage="All remaining funds will be transferred to the {destination} within a few days after exit epoch reached"
+                          values={{
+                            destination: (
+                              <strong>
+                                <FormattedMessage defaultMessage="validator index you choose below" />
+                              </strong>
+                            ),
+                          }}
+                        />
+                      </Text>
+                    </li>
+                  </ul>
+                </ModalContent>
+              )}
+
+              <div
+                style={{
+                  marginBottom: '1rem',
+                  marginTop: selectedValidator ? '0' : '1rem',
+                }}
+              >
+                <div style={{ marginBottom: '0.5rem' }}>
+                  <FormattedMessage defaultMessage="Which validator would you like to migrate funds to?" />
+                </div>
+                <ValidatorSelector
+                  validators={targetValidatorSet}
+                  selectedValidator={selectedValidator}
+                  setSelectedValidator={setSelectedValidator}
+                />
+              </div>
+
+              {selectedValidator && (
+                <div style={{ marginBlock: '1.5rem' }}>
+                  <Alert variant="warning">
+                    <AlertContent>
+                      <AlertIcon />
+                      <div>
+                        <Text>
+                          <strong>
+                            <FormattedMessage
+                              defaultMessage="Account {sourceIndex} will be exited from the network, and its full balance will be migrated to validator index {targetIndex}."
+                              values={{
+                                sourceIndex: sourceValidator.validatorindex,
+                                targetIndex: selectedValidator.validatorindex,
+                              }}
+                            />
+                          </strong>
+                        </Text>
+                        <Text style={{ fontSize: '1rem' }}>
+                          <FormattedMessage defaultMessage="The exiting validator should remain online until exit epoch is reached." />
+                        </Text>
+                      </div>
+                    </AlertContent>
+                  </Alert>
+                </div>
+              )}
+
+              {selectedValidator && (
+                <ModalContent>
+                  <div
+                    style={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      gap: '1rem',
+                    }}
+                  >
+                    <div
+                      style={{
+                        background: '#fff',
+                        padding: '1rem',
+                        maxWidth: '100%',
+                        borderRadius: '8px',
+                        border: '2px solid darkred',
+                      }}
+                    >
+                      <Heading
+                        level={3}
+                        style={{ textTransform: 'uppercase', color: 'darkred' }}
+                      >
+                        <FormattedMessage defaultMessage="From" />
+                      </Heading>
+                      <Text>
+                        <strong>
+                          <FormattedMessage defaultMessage="Index" />:{' '}
+                          {selectedValidator.validatorindex}
+                        </strong>
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: '14px',
+                          color: '#555',
+                          marginBottom: '0.5rem',
+                        }}
+                      >
+                        <Hash>{selectedValidator.pubkey}</Hash>
+                      </Text>
+                      <Text>
+                        <FormattedMessage defaultMessage="Current balance" />:{' '}
+                        {selectedValidator.coinBalance} {TICKER_NAME}
+                      </Text>
+                      <Text>
+                        <FormattedMessage defaultMessage="Ending balance" />:{' '}
+                        <strong>0 {TICKER_NAME}</strong>
+                      </Text>
+                    </div>
+                    <div
+                      style={{
+                        background: '#fff',
+                        padding: '1rem',
+                        maxWidth: '100%',
+                        borderRadius: '8px',
+                        border: '2px solid darkgreen',
+                      }}
+                    >
+                      <Heading
+                        level={3}
+                        style={{
+                          textTransform: 'uppercase',
+                          color: 'darkgreen',
+                        }}
+                      >
+                        <FormattedMessage defaultMessage="To" />
+                      </Heading>
+                      <Text>
+                        <strong>
+                          <FormattedMessage defaultMessage="Index" />:{' '}
+                          {sourceValidator.validatorindex}
+                        </strong>
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: '14px',
+                          color: '#555',
+                          marginBottom: '0.5rem',
+                        }}
+                      >
+                        <Hash>{sourceValidator.pubkey}</Hash>
+                      </Text>
+                      <Text>
+                        <FormattedMessage defaultMessage="Current balance" />:{' '}
+                        {sourceValidator.coinBalance} {TICKER_NAME}
+                      </Text>
+                      <Text>
+                        <FormattedMessage defaultMessage="Ending balance" />:{' '}
+                        <strong>
+                          {sourceValidator.coinBalance +
+                            selectedValidator.coinBalance}{' '}
+                          {TICKER_NAME}
+                        </strong>
+                      </Text>
+                    </div>
+                  </div>
+                </ModalContent>
+              )}
+            </ModalBody>
           </Box>
-          <Box
-            as="footer"
-            gap="small"
-            direction="row"
-            align="center"
-            justify="center"
-            pad={{ top: 'medium', bottom: 'small' }}
-          >
+
+          <Box as="footer" pad="1rem">
             <Button
-              label="Cancel"
-              onClick={() => closeSelectValidatorModal()}
-            />
-            <Button
-              disabled={!selectedTargetValidator}
-              label="Migrate funds"
-              onClick={() => createConsolidationTransaction()}
+              fullWidth
+              destructive
+              disabled={!selectedValidator}
+              label={
+                selectedValidator ? (
+                  <FormattedMessage defaultMessage="I understand the consequences, consolidate funds" />
+                ) : (
+                  <FormattedMessage defaultMessage="Migrate funds" />
+                )
+              }
+              onClick={createConsolidationTransaction}
             />
           </Box>
         </Layer>
       )}
 
-      {showTxModal && selectedTargetValidator && (
+      {showTxModal && selectedValidator && (
         <TransactionStatusModal
           headerMessage={
-            <FormattedMessage
-              defaultMessage="Consolidate {index} into {otherIndex}"
-              values={{
-                index: selectedTargetValidator.validatorindex,
-                otherIndex: sourceValidator.validatorindex,
-              }}
-            />
+            <>
+              <span>{sourceValidator.validatorindex}</span>{' '}
+              <span style={{ transform: locale === 'ar' ? 'scale(-1)' : '' }}>
+                {String.fromCodePoint(0x27a0)}
+              </span>
+              <span>{selectedValidator.validatorindex}</span>
+            </>
           }
           txHash={txHash}
           transactionStatus={transactionStatus}
-          onClose={() => setShowTxModal(false)}
+          onClose={handleClose}
           handleRetry={createConsolidationTransaction}
         />
       )}
 
-      <Button
-        label="Migrate funds"
-        destructive
-        onClick={() => confirmConsolidate()}
-      />
+      <Button label="Migrate funds" destructive onClick={confirmConsolidate} />
     </>
   );
 };
